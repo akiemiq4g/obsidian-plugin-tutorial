d8bde900e805bd78221725170770be160113a7f5
Akiem Watley <akiemiq4g@icloud.com>
2024-05-06 05:29:52 -0400

    count lines

--- main.ts
+++ main.ts
@@ -5,14 +5,10 @@ export default class ExamplePlugin extends Plugin {
 
 	onload() {
 		this.statusBarTextElement = this.addStatusBarItem().createEl('span')
-		this.statusBarTextElement.innerText = "Hello"
-
+		this.readActiveFileAndUpdateLineCount();
+	
 		this.app.workspace.on('active-leaf-change', async () => {
-			const file = this.app.workspace.getActiveFile()
-			if (file) {
-				const content = await this.app.vault.read(file);
-				this.updateLineCount(content);
-			}
+			this.readActiveFileAndUpdateLineCount();
 		});
 
 		this.app.workspace.on('editor-change', editor => {
@@ -25,5 +21,15 @@ export default class ExamplePlugin extends Plugin {
 		const count = fileContent ? fileContent.split(/\r\n|\r|\n/).length : 0;
 		const linesWord = count === 1 ? "line" : "lines";
 		this.statusBarTextElement.textContent = `${count} ${linesWord}`;
+	}
+
+	private async readActiveFileAndUpdateLineCount(){
+		const file = this.app.workspace.getActiveFile()
+		if (file) {
+			const content = await this.app.vault.read(file);
+			this.updateLineCount(content);
+		} else {
+			this.updateLineCount(undefined);
+		}
 	}
 }
