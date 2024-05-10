ed7a310a8f4a730c275716c8c924c72fe04c8e1d
Akiem Watley <akiemiq4g@icloud.com>
2024-05-09 20:17:53 -0400

    Add Command with this.addCommand 9 May 2024

--- main.ts
+++ main.ts
@@ -1,35 +1,92 @@
-import { Plugin } from "obsidian";
+import { 
+	Editor,
+	Notice,
+	Plugin,
+	request, 
+} from "obsidian";
 
-export default class ExamplePlugin extends Plugin {
-	statusBarTextElement: HTMLSpanElement;
+interface ApiChunk{
+    id: string;
+    object: string;
+    created: number;
+    model: string;
+    choices: [{
+        finish_reason: null;
+        delta: {
+            content: string | null; 
+        };
+        message: {
+			role: string | null;
+			content: string | null;
 
-	onload() {
-		this.statusBarTextElement = this.addStatusBarItem().createEl('span')
-		this.readActiveFileAndUpdateLineCount();
-	
-		this.app.workspace.on('active-leaf-change', async () => {
-			this.readActiveFileAndUpdateLineCount();
-		});
+		};
+        sources: [];
+        index: Number;
+    }]
+}
 
-		this.app.workspace.on('editor-change', editor => {
-			const content = editor.getDoc().getValue();
-			this.updateLineCount(content);
+let stream = true;
+let response = '';
+export default class AAAppPlugin extends Plugin {
+
+	async onload() {
+		this.addRibbonIcon("wand", "AAAppPlugin", () => {
+			request({
+				url: "http://localhost:8001/v1/completions",
+				method: "POST",
+				body: JSON.stringify({
+					use_context: true,
+					prompt: "Hello, PrivateGPT, I'm over at Obsidian",
+					stream: stream,
+				})
+			})
+			.then((data) => { 
+				if (stream) {
+					let response: String = "";
+					data.split("}\n").forEach(line => {
+						if (!line.contains("[DONE]")) {
+							let resChunk = JSON.parse(line.slice(6)+'}').choices[0].delta.content
+							response += resChunk;
+
+
+						}
+						
+						console.log(String(response))
+					})
+				} else {
+					const response:ApiChunk = JSON.parse(data);
+					return response.choices[0].message.content;
+					
+				}
+
+				
+
+				
+				
+			})
 		})
-	}
+			
+		this.addCommand({
+			id: "prompt",
+			name: "Prompt",
+			editorCallback: (editor: Editor) => {
+				response = "Hello, World!";
+				const userRequest = editor.getSelection();
+				new Notice("Your request is my command.");
+				console.log(userRequest);
+				editor.replaceRange(
+					response,
+					editor.getCursor()
+				);
+				
+			}
+		})
+	};
 
-	private updateLineCount(fileContent?: string) {
-		const count = fileContent ? fileContent.split(/\r\n|\r|\n/).length : 0;
-		const linesWord = count === 1 ? "line" : "lines";
-		this.statusBarTextElement.textContent = `${count} ${linesWord}`;
-	}
 
-	private async readActiveFileAndUpdateLineCount(){
-		const file = this.app.workspace.getActiveFile()
-		if (file) {
-			const content = await this.app.vault.read(file);
-			this.updateLineCount(content);
-		} else {
-			this.updateLineCount(undefined);
-		}
-	}
+
+	async onunload() {
+		console.log("AAAppPlugin unloaded");
+    }
+	
 }
